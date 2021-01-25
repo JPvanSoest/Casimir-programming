de3e4e6e03b4566460afce2a10f590d444505f59
Jean-Paul <j.vansoest-1@tudelft.nl>
2021-01-25 13:45:39 +0100

    Another function, this one for the area of a circle

--- test.py
+++ test.py
@@ -2,10 +2,17 @@ import numpy as np
 
 print("Hello World")
 
+def surf(r):
+    s = np.pi*r**2
+    return(s)
+
 def circumf(r):
     c = 2*np.pi*r
     return(c)
 
 radius = 6
 circumference = circumf(radius)
+surface = surf(radius)
+
+
 
